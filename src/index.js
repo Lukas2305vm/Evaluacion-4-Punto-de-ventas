9afb44b795c70e2d47961693a7621b1d2bb4c039
Lukas Videla <94393404+Lukas2305vm@users.noreply.github.com>
2022-07-14 19:32:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import  App  from "./App";
 
-const API_URL = ""
+const apiUrl= ""
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
